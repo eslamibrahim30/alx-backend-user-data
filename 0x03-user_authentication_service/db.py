ff906b07e0247fdecf846078c44ba9c667b2929b
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-04-26 01:08:23 +0300

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email, hashed_password) -> User:
         """
         The method saves a new user to the database.
         """
